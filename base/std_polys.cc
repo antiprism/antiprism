b910f6ca97f841022a54e75f156fbd1c00185e37
Adrian Rossiter <adrian@antiprism.com>
2011-06-14 11:59:17 +0200

    Uniform/Johnson models: don't add edge colours

--- base/std_polys.cc
+++ base/std_polys.cc
@@ -432,8 +432,6 @@ void set_resource_polygon_color(geom_if &geom)
       clrng.add_cmap(cmap);
       clrng.f_avg_angle(true);
       clrng.v_avg_angle(true);
-      geom.add_missing_impl_edges();
-      clrng.e_one_col(vec3d(1.0, 1.0, 1.0));
    }
 }
 
